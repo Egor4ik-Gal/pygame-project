04f312a58c4a97bf69ed186ee14ef0d0a1e88e9b
Masechka <kleyzermp@gmail.com>
2022-12-17 23:43:05 +0300

    mini-game cathing balls demo

--- main.py
+++ main.py
@@ -1,1 +1,186 @@
-print()
+import pygame
+import os
+from random import randrange
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = load_image("ball.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 150 + randrange(315)
+        self.rect.y = 0
+
+
+class CatchingBalls:
+    def __init__(self, screen):
+        self.screen = screen
+        pygame.draw.rect(screen, (255, 255, 255), (150, 0, 350, 350))
+        pygame.draw.rect(screen, (200, 170, 100), (150, 400, 480, 80))
+        font = pygame.font.Font(None, 30)
+        text = font.render("Поймайте 30 шариков!", True, (0, 0, 0))
+        text_x = 110
+        text_y = 10
+        screen.blit(text, (text_x, text_y))
+
+        self.all_sprites = pygame.sprite.Group()
+        self.basket = pygame.sprite.Sprite()
+        self.basket.image = load_image("basket.png")
+        self.basket.rect = self.basket.image.get_rect()
+        self.all_sprites.add(self.basket)
+
+        self.basket.rect.x = 150
+        self.basket.rect.y = 250
+
+        self.hard1 = pygame.sprite.Sprite()
+        self.hard1.image = load_image("health.png")
+        self.hard1.rect = self.basket.image.get_rect()
+        self.all_sprites.add(self.hard1)
+
+        self.hard1.rect.x = 465
+        self.hard1.rect.y = 10
+
+        self.hard2 = pygame.sprite.Sprite()
+        self.hard2.image = load_image("health.png")
+        self.hard2.rect = self.basket.image.get_rect()
+        self.all_sprites.add(self.hard2)
+
+        self.hard2.rect.x = 435
+        self.hard2.rect.y = 10
+
+        self.hard3 = pygame.sprite.Sprite()
+        self.hard3.image = load_image("health.png")
+        self.hard3.rect = self.basket.image.get_rect()
+        self.all_sprites.add(self.hard3)
+
+        self.hard3.rect.x = 405
+        self.hard3.rect.y = 10
+
+        self.all_sprites.draw(screen)
+
+        # pygame.display.flip()
+        if self.playing():
+            font = pygame.font.Font(None, 40)
+            text = font.render("Вы победили!!!", True, (100, 255, 100))
+            text_x = 210
+            text_y = 150
+            text_w = text.get_width()
+            text_h = text.get_height()
+            pygame.draw.rect(screen, (0, 0, 0), (text_x - 10, text_y - 10,
+                                                 text_w + 20, text_h + 20))
+            screen.blit(text, (text_x, text_y))
+        else:
+            font = pygame.font.Font(None, 40)
+            text = font.render("Вы проиграли!!!", True, (100, 255, 100))
+            text_x = 210
+            text_y = 150
+            text_w = text.get_width()
+            text_h = text.get_height()
+            pygame.draw.rect(screen, (0, 0, 0), (text_x - 10, text_y - 10,
+                                                 text_w + 20, text_h + 20))
+            screen.blit(text, (text_x, text_y))
+
+    def playing(self):
+        clock = pygame.time.Clock()
+        running = True
+
+        CREATINGBALLS = pygame.USEREVENT + 1
+        FALL = pygame.USEREVENT + 2
+        pygame.time.set_timer(CREATINGBALLS, 900)
+        count_of_fallen_balls = 0
+        count_of_catched_balls = 0
+        count = 0
+        list_of_balls = []
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == CREATINGBALLS:
+                    list_of_balls.append(Ball(self.all_sprites))
+                    count += 1
+                    if count == 33:
+                        pygame.time.set_timer(CREATINGBALLS, 0)
+                if event.type == FALL:
+                    for i in range(len(list_of_balls)):
+                        list_of_balls[i].rect.y += 1
+                        if list_of_balls[i].rect.y == 310:
+                            count_of_fallen_balls += 1
+                            list_of_balls[i].kill()
+                            list_of_balls.remove(list_of_balls[i])
+                            if count_of_fallen_balls == 1:
+                                self.hard3.kill()
+                            if count_of_fallen_balls == 2:
+                                self.hard2.kill()
+                            if count_of_fallen_balls == 3:
+                                self.hard1.kill()
+                            break
+                        if list_of_balls[i].rect.center[1] in range(270, 310) and \
+                                list_of_balls[i].rect.center[0] in range(self.basket.rect.x, self.basket.rect.x + 100):
+                            list_of_balls[i].kill()
+                            count_of_catched_balls += 1
+                            list_of_balls.remove(list_of_balls[i])
+                            break
+                if count == 1:
+                    pygame.time.set_timer(FALL, 4)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                if self.basket.rect.x > 145:
+                    self.basket.rect.x -= 1
+                    clock.tick(300)
+            if keys[pygame.K_RIGHT]:
+                if self.basket.rect.x < 435:
+                    self.basket.rect.x += 1
+                    clock.tick(300)
+            pygame.draw.rect(self.screen, (255, 255, 255), (150, 0, 350, 350))
+            pygame.draw.rect(self.screen, (200, 170, 100), (150, 300, 350, 60))
+            font = pygame.font.Font(None, 30)
+            text = font.render("Поймайте 30 шариков!", True, (0, 0, 0))
+            text_x = 155
+            text_y = 10
+            self.screen.blit(text, (text_x, text_y))
+
+            stroka = f'Вы поймали {count_of_catched_balls}/30'
+            text2 = font.render(stroka, True, (0, 0, 0))
+            text2_x = 155
+            text2_y = 35
+            self.screen.blit(text2, (text2_x, text2_y))
+            self.all_sprites.draw(self.screen)
+            pygame.display.flip()
+            if count_of_fallen_balls >= 3:
+                return False
+            if count_of_catched_balls == 30:
+                return True
+
+
+def running():
+    pygame.init()
+    size = 650, 350
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Игра')
+    CatchingBalls(screen)
+    pygame.display.flip()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    running()
