f81e3036c71a246c05f5d248a207b3a3b3788d3c
Egor <galuzoegor@yandex.ru>
2022-12-17 21:16:11 +0300

    room-and-char

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print()
+import pygame
+
+pygame.init()
+size = w, h = 650, 350
+pygame.display.set_caption('room1')
+clock = pygame.time.Clock()
+fps = 60
+speed = 10
+bg = pygame.image.load(r'data\room1.png')
+pr1 = pygame.image.load(r'data\pers1.png')
+pr2 = pygame.image.load(r'data\pers1.1.png')
+pr2.set_colorkey((255, 255, 255))
+all_sprites = pygame.sprite.Group()
+screen = pygame.display.set_mode(size)
+screen.blit(pr1, (0, 0))
+# class Person(pygame.sprite.Sprite):
+#     def __init__(self):
+person = pygame.sprite.Sprite(all_sprites)
+person.image = pr1
+person.rect = person.image.get_rect()
+person.rect.x = 0
+person.rect.y = h - 156 - 49
+
+running = True
+while running:
+    for event in pygame.event.get():
+        screen.blit(bg, (0, 0))
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == 100:
+                person.rect.x += speed
+                person.image = pr1
+            if event.key == 97:
+                person.rect.x -= speed
+                person.image = pr2
+        all_sprites.draw(screen)
+        clock.tick(fps)
+        pygame.display.flip()
+pygame.quit()
