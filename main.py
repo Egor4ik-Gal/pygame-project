3688223ee3d7ffe9d45b394f3a6249a1ec0f8382
Egor <galuzoegor@yandex.ru>
2022-12-23 22:59:05 +0300

    char-walk

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ speed = 10
 bg = pygame.image.load(r'data\room1.png')
 pr1 = pygame.image.load(r'data\pers1.png')
 pr2 = pygame.image.load(r'data\pers1.1.png')
+pr3 = pygame.image.load(r'data\pers1.2.png')
+pr4 = pygame.image.load(r'data\pers1.22.png')
 pr2.set_colorkey((255, 255, 255))
 all_sprites = pygame.sprite.Group()
 screen = pygame.display.set_mode(size)
@@ -19,7 +21,27 @@ person.rect = person.image.get_rect()
 person.rect.x = 0
 person.rect.y = h - 205
 
+
+def pers(flag1, rotate):
+    global flag
+    if flag1 is True:
+        if rotate == 'r':
+            flag = False
+            return pr3
+        else:
+            flag = False
+            return pr4
+    else:
+        if rotate == 'r':
+            flag = True
+            return pr1
+        else:
+            flag = True
+            return pr2
+
+
 running = True
+flag = True
 while running:
     for event in pygame.event.get():
         screen.blit(bg, (0, 0))
@@ -29,11 +51,11 @@ while running:
             if event.key == 100 or event.key == 1073741903:
                 if person.rect.x + 78 <= w:
                     person.rect.x += speed
-                person.image = pr1
+                person.image = pers(flag, 'r')
             if event.key == 97 or event.key == 1073741904:
                 if person.rect.x != 0:
                     person.rect.x -= speed
-                person.image = pr2
+                person.image = pers(flag, 'l')
         all_sprites.draw(screen)
         clock.tick(fps)
         pygame.display.flip()
